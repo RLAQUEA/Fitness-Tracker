e9e509c91d439c7069296acbc498c369a1e61815
rlaquea <rlaquea14@gmail.com>
2021-08-18 18:51:03 -0400

    fixes, & readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Fitness-Tracker
+# Fitness Tracker 
+
+## MongoDB Atlas Application
+
+This application is a fitness tracker that allows the user to track cardio and resistance training workouts.  The user enters the paramaters for the workout, i.e., duration, weight, total time, etc. and can it to the application's database.  Once they have added the workout, they can complete the workout and their created/completed workouts are shown on the user's dashboard.  The dashboard also shows the last seven days' workouts, and their stats for their previous workouts. 
--- controllers/api.js
+++ controllers/api.js
@@ -1,71 +1,125 @@
-const router = require("express").Router();
+
 const Workout = require("../models/workout.js");
+const router = require("express").Router();
+router.get("/workouts", async (req, res) => {
+  try {
 
-//POST route to add new workout
-router.post("/workouts", (req, res) => {
-Workout.create({})
-.then(createdWorkout => {
-res.json(createdWorkout)
-})
-.catch((err) => {
-res.json(err);
-});
-})
-
-//PUT route to update workout
-router.put("/workouts/:id", ({ body, params }, res) => {
-Workout.findByIdAndUpdate(params.id, 
-{$push: {exercises: body}}, 
-{new: true, runValidators: true})
-.then((myWorkouts) => {
-  res.json(myWorkouts);
-})
-.catch((err) => {
-  res.json(err);
-});
-})
-
-
-//Past week of workouts
-router.get('/workouts', (req, res) => {
-    Workout.aggregate([
+    const data = await Workout.aggregate([
       {
         $addFields: {
           totalDuration: {
-            $sum: '$exercises.duration',
+            $sum: "$exercises.duration",
           },
         },
       },
-    ])
-      .then((myWorkouts) => {
-        res.json(myWorkouts);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
+    ]);
+    res.json(data);
+  }
+  catch (err) { res.json(err); }
+});
+
+router.post("/workouts", async ({ body }, res) => {
+  try {
+    const data = Workout.create(body);
   
+    res.json(data);
+  }
+  catch (err) { res.json(err); }
+});
 
+router.put("/workouts/:id", async ({ body, params }, res) => {
+  try{
+    const data = await Workout.findByIdAndUpdate(
+      { _id: `${params.id}` },
+      { $push: { exercises: body } }
+    );
+    res.json(data);
+  }
+  catch(err) { res.json(err); }
+});
 
-  router.get('/workouts/range', (req, res) => {
-    Workout.aggregate([
-      {
-        $addFields: {
-          totalDuration: {
-            $sum: '$exercises.duration',
-          },
-        },
-      },
-    ])
-      .sort({ _id: -1 })
-      .limit(7)
-      .then((myWorkouts) => {
-        console.log(myWorkouts);
-        res.json(myWorkouts);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
+router.get("/workouts/range", (req, res) => {
+  Workout.aggregate([
+    { $addFields: { totalDuration: { $sum: "$exercises.duration", 
+  }, 
+}, 
+}, 
+])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((data) => { res.json(data); })
+    .catch((err) => { res.json(err); });
+});
 
 module.exports = router;
+
+
+
+// const router = require("express").Router();
+// const Workout = require("../models/workout.js");
+
+// //POST route to add new workout
+// router.post("/workouts", (req, res) => {
+// Workout.create({})
+// .then(createdWorkout => {
+// res.json(createdWorkout)
+// })
+// .catch((err) => {
+// res.json(err);
+// });
+// })
+
+// //PUT route to update workout
+// router.put("/workouts/:id", ({ body, params }, res) => {
+// Workout.findByIdAndUpdate(params.id, 
+// {$push: {exercises: body}}, 
+// {new: true, runValidators: true})
+// .then((myWorkouts) => {
+//   res.json(myWorkouts);
+// })
+// .catch((err) => {
+//   res.json(err);
+// });
+// })
+
+// //Past week of workouts
+// router.get('/workouts', (req, res) => {
+//     Workout.aggregate([
+//       {
+//         $addFields: {
+//           totalDuration: {
+//             $sum: '$exercises.duration',
+//           },
+//         },
+//       },
+//     ])
+//       .then((myWorkouts) => {
+//         res.json(myWorkouts);
+//       })
+//       .catch((err) => {
+//         res.json(err);
+//       });
+//   });
+
+//   router.get('/workouts/range', (req, res) => {
+//     Workout.aggregate([
+//       {
+//         $addFields: {
+//           totalDuration: {
+//             $sum: '$exercises.duration',
+//           },
+//         },
+//       },
+//     ])
+//       .sort({ _id: -1 })
+//       .limit(7)
+//       .then((myWorkouts) => {
+//         console.log(myWorkouts);
+//         res.json(myWorkouts);
+//       })
+//       .catch((err) => {
+//         res.json(err);
+//       });
+//   });
+
+// module.exports = router;
